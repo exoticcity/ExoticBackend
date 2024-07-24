f98f608ba9d9294b268612452e6da4cc837dfdeb
exoticcity <exoticcity121@gmail.com>
2024-07-24 17:50:14 +0500

    Update settings.py

--- exotic/settings.py
+++ exotic/settings.py
@@ -47,7 +47,8 @@ CORS_ALLOWED_ORIGINS = [
     'https://exoticshop.eu',
     'http://exoticshop.eu',
     'http://exoticbackend.exoticshop.eu',
-    'https://exoticbackend.exoticshop.eu'
+    'https://exoticbackend.exoticshop.eu',
+    'exoticcity-a0dfd0ddc0h2h9hb.northeurope-01.azurewebsites.net'
     # Add other allowed origins as needed
 ]
 
