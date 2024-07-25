2faf29a96b56c606219450b69f5476897c87bb25
exoticcity <exoticcity121@gmail.com>
2024-07-25 16:20:35 +0500

    Update views.py

--- items/views.py
+++ items/views.py
@@ -516,7 +516,7 @@ class CartAPIViewset(viewsets.ViewSet):
                 response = getPrice(self, Product.objects.get(ItemNo=item["itemNo"]).ItemNo, Customer.objects.get(customer_id=pk).CustomerPriceGroup, item_created.quantity)
                 # response = requests.get(f'https://exoticcity-a0dfd0ddc0h2h9hb.northeurope-01.azurewebsites.net/items/getPrice/{Product.objects.get(ItemNo=item["itemNo"]).ItemNo}/{Customer.objects.get(customer_id=pk).CustomerPriceGroup}/{item_created.quantity}')
                 print(response)
-                data = response.json()
+                data = response
                 item_created.total_amount_excluding_vat = round(round(float(data['price']), 2) * item_created.quantity , 2)
                 item_created.total_amount_including_vat = round(( ((round(float(data['price']), 2))*((item_created.vat_rate/100)*item_created.quantity)) + (round(round(float(data['price']), 2) * item_created.quantity , 2)) ), 2)
                 item_created.save()
